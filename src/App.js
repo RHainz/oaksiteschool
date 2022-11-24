21bd3078ccb0eb74b8c89bfb0940321389899efb
RHainz <rogerhainz@gmail.com>
2022-11-24 10:37:41 -0300

    Main in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import {Main} from "./components/Main"
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+
+  return (<>
+  
+      <Main />
+  
+  </>);
 }
 
 export default App;
