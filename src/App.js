e3db908e14b06ffda870e23f37294bdaa25e71e5
RHainz <rogerhainz@gmail.com>
2023-01-03 14:08:32 -0300

    Whats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Main } from "./components/Main.js"
 import { Videos } from "./components/Videos.js"
 import { Sobre } from "./components/Sobre.js"
+import { Wazzah } from "./components/Wazzah.js"
 
 function App() {
   return (
     <>
+      <Wazzah />
       <Main />
       <Sobre />
       <Videos />
---
+++ src/components/Wazzah.js
@@ -0,0 +1,16 @@
+// import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+
+
+export function Wazzah () {
+
+
+    return (<>
+        
+        <div className="ml-80">
+        <a href="https://wa.me/551591582979" target="_blank">
+        Whatsapp
+        </a>
+        </div>
+
+        </>)
+}
